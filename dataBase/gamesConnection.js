c4130003651da5ef8b4441bc3b0ba44947dbefdf
Gaston Avila <gastonavila14@gmail.com>
2022-07-25 18:42:43 -0300

    Game endpoint developed

---
+++ dataBase/gamesConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+require('dotenv/config')
+const gamesConnectionDB= async()=>{
+    try {
+        await mongoose.connect(process.env.DB_URL_CONECTION_GAMES)
+        console.log('Te conectaste con exito a la base de datos de Games')
+    } catch (error) {
+        console.log('Algo salio mal, revise la conexion',error)
+        
+    }
+}
+gamesConnectionDB()
---
+++ routes/gamesRoutes.js
@@ -0,0 +1,13 @@
+const {Router} = require('express')
+const {getGame, sendGame, deleteGame, updateGame} = require('../controller/gamesController')
+const gamesRoutes = Router()
+
+gamesRoutes.get('/', getGame)
+
+gamesRoutes.post('/', sendGame)
+
+gamesRoutes.delete('/:id',deleteGame)
+
+gamesRoutes.put('/:id', updateGame)
+
+module.exports = gamesRoutes
